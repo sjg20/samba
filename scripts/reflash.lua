31a332cdaee624fc9506d19af597bb395486fcbd
andre <andre@8dfeb43b-0a0a-0410-ae3d-a7a6a9434e40>
2009-04-07 03:28:41 +0000

    Removed old lua scripts Updated scripts to new release paths
    git-svn-id: file:///work/snapper/svn/utility/samba-script@3299 8dfeb43b-0a0a-0410-ae3d-a7a6a9434e40

--- scripts/reflash.lua
+++
@@ -1,7 +0,0 @@
-a = at91.open(0x3eb, 0x6124)
-v = a:version()
-print ("Version: " .. v)
-a:nand_erase(0x0, 0x60000)
-a:nand_write_file(0x0, "/home/andre/snapper/Bootstrap-v1.9/board/sn9260/sn9260_bootstrap.bin")
-a:nand_write_file(0x20000, "/home/andre/snapper/u-boot-1.1.6/u-boot.bin")
-a:go(0x10000) -- start the standard rom boot
